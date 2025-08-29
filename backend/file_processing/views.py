cf46cc1741c6b5a2bbc9c9c9feb9fa8a3958e09d
ChrisW-priv <chris.watras@gmail.com>
2025-08-29 13:13:24 +0200

    make use of some super basic processing and stop at the info to process section digest file

--- backend/file_processing/views.py
+++ backend/file_processing/views.py
@@ -1,7 +1,12 @@
+import logging
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, serializers
 from django.conf import settings
+from content_extraction.process import process_file
+
+
+logger = logging.getLogger(__name__)
 
 
 class EventarcMessageSerializer(serializers.Serializer):
@@ -31,11 +36,16 @@ class EventarcHandler(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         # You can process the validated data here
         # For demonstration, just return the validated data
-        filename = serializer.validated_data['name']
+        filename: str = serializer.validated_data['name']
+        if not filename.startswith(settings.UPLOAD_FOLDER_NAME):
+            """
+            Do not process files that were uploaded to a diff folder than the upload folder
+            """
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
         full_name = settings.PRIVATE_MOUNT / filename
-        print(f'Received object {serializer.validated_data}')
-        print(f'We expect to be able to open the file with {full_name=}')
-        with open(full_name) as f:
-            data = f.read()
-        print(f'Opened the file and got {data=}')
+        output_dir = settings.PRIVATE_MOUNT / 'process-results'
+        process_file(full_name, output_dir)
+        section_digest_file = output_dir / 'sections.jsonl'
+        logger.info(f'We should now try to process the {section_digest_file=}')
         return Response(status=status.HTTP_204_NO_CONTENT)
