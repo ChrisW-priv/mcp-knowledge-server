400e4b698c222ccab6786a21eabf862dce16ee53
ChrisW-priv <chris.watras@gmail.com>
2025-09-01 10:38:05 +0200

    chore: add some logging to allow debuging in prod

--- backend/file_processing/google_auth.py
+++ backend/file_processing/google_auth.py
@@ -3,6 +3,12 @@ from rest_framework import exceptions
 from google.oauth2 import id_token
 from google.auth.transport import requests
 
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
 class ServiceAccountUser:
     def __init__(self, email):
         self.username = email
@@ -16,6 +22,7 @@ class ServiceAccountUser:
 class GoogleOIDCAuthentication(BaseAuthentication):
     def authenticate(self, request):
         auth_header = request.META.get('HTTP_AUTHORIZATION')
+        logger.info('Started to authenticate with custom OIDC Authentication')
         if not auth_header or not auth_header.startswith('Bearer '):
             return None  # No Bearer token, let other auth classes try
 
@@ -23,10 +30,12 @@ class GoogleOIDCAuthentication(BaseAuthentication):
         try:
             # Use your actual service URL as the audience!
             audience = 'https://knowledge-server-obr76apg5a-ez.a.run.app/'
+            logger.info('Trying to verify the oauth2 token')
             idinfo = id_token.verify_oauth2_token(token, requests.Request(), audience)
             email = idinfo.get('email')
             if not email:
                 raise exceptions.AuthenticationFailed('No email in token')
+            logger.info(f'Successfuly retrieved the {email=}')
             user = ServiceAccountUser(email)
             return (user, None)
         except Exception as e:
