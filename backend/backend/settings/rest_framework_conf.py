2fa3a73780c014f05dc353b256e445b1e95b5282
ChrisW-priv <chris.watras@gmail.com>
2025-09-11 12:07:12 +0200

    Turn off the Permission Check for now

--- backend/backend/settings/rest_framework_conf.py
+++ backend/backend/settings/rest_framework_conf.py
@@ -8,6 +8,6 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
-        "content_access_control.permissions.SubjectHasUrlPermission",
+        # "content_access_control.permissions.SubjectHasUrlPermission",
     ],
 }
