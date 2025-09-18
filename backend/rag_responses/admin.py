53ecf576a81b63050f0042474da6d8bd00fbd932
ChrisW-priv <chris.watras@gmail.com>
2025-09-18 18:23:41 +0200

    chore: add models to admin to allow manual inspection

--- backend/rag_responses/admin.py
+++ backend/rag_responses/admin.py
@@ -1,1 +1,13 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Chat, ChatMessage
+
+
+class MessageInline(admin.TabularInline):
+    model = ChatMessage
+    extra = 0
+
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    inlines = (MessageInline,)
+    search_fields = ["id"]
