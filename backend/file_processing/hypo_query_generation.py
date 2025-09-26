79f3b4f0de5b2c354363dbb4ad83bf2a4837e4dc
ChrisW-priv <chris.watras@gmail.com>
2025-09-26 12:15:40 +0200

    feat: save chunks from the file processing

---
+++ backend/file_processing/hypo_query_generation.py
@@ -0,0 +1,14 @@
+"""Create dspy modules that will take the dictionary:
+
+{
+    "title": "title",
+    "text": "text",
+    "language": "language",
+    "section_digest": {
+        "digest": "digest",
+        "start": "start",
+        "end": "end"
+    },
+}
+
+"""
--- backend/file_processing/views/eventarc.py
+++ backend/file_processing/views/eventarc.py
@@ -116,7 +116,14 @@ def process_file_to_sections(object_name: str):
         logger.error(f"Error processing file {object_name}: {e}")
         rmtree(output_dir)
         return None
-    return chunks
+    for chunk in chunks:
+        digest_hash: str = chunk.get("digest_hash")
+        if not digest_hash:
+            raise ValueError("No digest hash found")
+        filename = digest_hash + ".json"
+        file_path = output_dir / filename
+        with open(file_path, "w") as f:
+            json.dump(chunk, f)
 
 
 def insert_vector(
